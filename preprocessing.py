2d94d69724354e6e53a80ce9ff84e14485ecb07a
Rémi Boutonnier <remi.boutonnier@student-cs.fr>
2023-10-16 20:15:04 +0200

    Naming features and plotting most important features in linear regression

--- preprocessing.py
+++ preprocessing.py
@@ -1,39 +1,36 @@
-import pandas as pd
+import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import OrdinalEncoder
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.metrics import mean_squared_error
-
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
 
 # Fixing randomness to get reproducible results
 random_seed = 0
 np.random.seed(random_seed)
 
-data = pd.read_csv('./regularite-mensuelle-tgv-aqst.csv', delimiter=';')
+data = pd.read_csv("./regularite-mensuelle-tgv-aqst.csv", delimiter=";")
 explicative_columns = [
-    'date',
-    'service',
-    'gare_depart',
-    'gare_arrivee',
-    'duree_moyenne',
-    'nb_train_prevu'
+    "date",
+    "service",
+    "gare_depart",
+    "gare_arrivee",
+    "duree_moyenne",
+    "nb_train_prevu",
 ]
 
 target_columns = [
-    'retard_moyen_arrivee',
-    'prct_cause_externe',
-    'prct_cause_infra',
-    'prct_cause_gestion_trafic',
-    'prct_cause_materiel_roulant',
-    'prct_cause_gestion_gare',
-    'prct_cause_prise_en_charge_voyageurs'
+    "retard_moyen_arrivee",
+    "prct_cause_externe",
+    "prct_cause_infra",
+    "prct_cause_gestion_trafic",
+    "prct_cause_materiel_roulant",
+    "prct_cause_gestion_gare",
+    "prct_cause_prise_en_charge_voyageurs",
 ]
 
 X = data[explicative_columns]
@@ -43,29 +40,22 @@ y_causes = data[target_columns[1:]]
 # Focus on delay for now
 X_train, X_test, y_train, y_test = train_test_split(X, y_delay, test_size=0.2)
 
-# Numerical features
-numeric_features = [
-    'duree_moyenne',
-    'nb_train_prevu'
-]
-num_pipeline = Pipeline([
-    ('std_scaler', StandardScaler())
-])
+# Numeric features
+numeric_features = ["duree_moyenne", "nb_train_prevu"]
+num_pipeline = Pipeline([("std_scaler", StandardScaler())])
 
 # Categorical features
-categorical_features = [
-    'service',
-    'gare_depart',
-    'gare_arrivee'
-]
+categorical_features = ["service", "gare_depart", "gare_arrivee"]
 
 # Create and apply the preprocessing pipeline
 
-data_transformer = ColumnTransformer([
-    ('numerical', num_pipeline, numeric_features),
-    ('date', OrdinalEncoder(), ['date']),
-    ('categorical', OneHotEncoder(), categorical_features)
-])
+data_transformer = ColumnTransformer(
+    [
+        ("numerical", num_pipeline, numeric_features),
+        ("date", OrdinalEncoder(), ["date"]),
+        ("categorical", OneHotEncoder(), categorical_features),
+    ]
+)
 
 X_train = data_transformer.fit_transform(X_train)
 X_test = data_transformer.transform(X_test)
@@ -85,3 +75,30 @@ reg.fit(X_train, y_train)
 y_test_pred = reg.predict(X_test)
 rmse_test = np.sqrt(mean_squared_error(y_true=y_test, y_pred=y_test_pred))
 print(rmse_test)
+
+
+# Get the feature names and concatenate them
+one_hot_encoder = data_transformer.named_transformers_["categorical"]
+one_hot_feature_names = one_hot_encoder.get_feature_names_out(categorical_features)
+numeric_feature_names = numeric_features
+date_feature_name = ["date"]
+all_feature_names = np.concatenate(
+    [numeric_feature_names, date_feature_name, one_hot_feature_names]
+)
+
+# Get the greatest coefficients (using absolute values)
+coefficients = pd.DataFrame({"feature_name": all_feature_names, "coef": np.transpose(reg.coef_)})
+
+# Sort the DataFrame by the absolute value of coefficients in descending order
+coefficients = coefficients.sort_values(by="coef", key=abs, ascending=False)
+
+# Get the top 10 coefficients
+top_10_coefficients = coefficients.head(10)
+
+# Plot the coefficients
+plt.figure(figsize=(8, 6))
+plt.barh(top_10_coefficients["feature_name"], top_10_coefficients["coef"])
+plt.xlabel("Coefficient")
+plt.ylabel("Feature")
+plt.title("Top 10 Features by Coefficient Magnitude")
+plt.show()
