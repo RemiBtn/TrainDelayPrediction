b64e57c8b37b7e46074608f692e69639394f075b
Rémi Boutonnier <remi.boutonnier@student-cs.fr>
2023-10-19 10:59:07 +0200

    Creating grid search cv tool

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,7 @@
 import numpy as np
 import xgboost as xgb
+from models import compare_models_with_grid_search_cv
+from preprocessing import load_and_process
 from sklearn.ensemble import (
     AdaBoostRegressor,
     ExtraTreesRegressor,
@@ -11,11 +13,60 @@ from sklearn.multioutput import MultiOutputRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 
-from preprocessing import load_and_process
-
 
 def main() -> None:
     x_train, y_train, x_test, y_test, _, _ = load_and_process()
+
+    # Define the parameter grids
+    linear_reg_params = {"fit_intercept": [True, False]}
+    knn_params = {
+        "n_neighbors": [3, 5, 7, int(np.sqrt(x_train.shape[0]))],
+        "weights": ["uniform", "distance"],
+    }
+    decision_tree_params = {"max_depth": [None, 10, 20, 30], "min_samples_split": [2, 5, 10]}
+    random_forest_params = {"n_estimators": [10, 50, 100, 200], "max_depth": [None, 10, 20, 30]}
+    extra_trees_params = {"n_estimators": [10, 50, 100, 200], "max_depth": [None, 10, 20, 30]}
+    ada_boost_params = {"n_estimators": [50, 100, 200], "learning_rate": [0.01, 0.1, 1]}
+    xgb_params = {
+        "n_estimators": [100, 200, 300],
+        "learning_rate": [0.01, 0.1, 0.2],
+        "max_depth": [3, 5, 7, 10, 20, 30],
+    }
+
+    # Perform grid search for each model
+    model_params = [
+        linear_reg_params,
+        knn_params,
+        decision_tree_params,
+        random_forest_params,
+        extra_trees_params,
+        ada_boost_params,
+        xgb_params,
+    ]
+    models = [
+        LinearRegression(),
+        KNeighborsRegressor(),
+        DecisionTreeRegressor(),
+        RandomForestRegressor(),
+        ExtraTreesRegressor(),
+        AdaBoostRegressor(),
+        xgb.XGBRegressor(),
+    ]
+    model_names = [
+        "LinearRegression",
+        "KNeighborsRegressor",
+        "DecisionTreeRegressor",
+        "RandomForestRegressor",
+        "ExtraTreesRegressor",
+        "AdaBoostRegressor",
+        "XGBRegressor",
+    ]
+
+    grid_search_results = compare_models_with_grid_search_cv(
+        x_train, y_train, x_test, y_test, model_params, models, model_names
+    )
+    print(grid_search_results)
+
     # Quick test with linear regression
     reg = LinearRegression()
     reg.fit(x_train, y_train)
